058b063bc8194d44a68f9e90f65337233646c7fa
Tran <andrewtttran@gmail.com>
2020-04-27 10:21:22 +1000

    radio buttons, selector forms, error checking

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -11,12 +11,21 @@ class ListingsController < ApplicationController
     end
 
     def new
+        @breeds=Breed.all #variable for new view for selector form
+        @sexes= Listing.sexes.keys #a way to access the snake sexes, as it's an enumerable
+        #if it was a modle we could so @sexes=Sex.all
         @listing = Listing.new
     end
 
     def create
         #finish logic for creating a record
-        Listing.create(listing_params)
+        @listing=Listing.create(listing_params)
+        if @listing.errors.any? #if any errors is true
+            set_breeds_and_sexes
+            render "new" #re-render new page
+        end
+        # puts @listing.errors.any? #check if any errors come up, will output true if there is error
+        # puts @listing.errors.full_messages #displays a fuller error message
     end
 
     def edit
@@ -38,7 +47,12 @@ class ListingsController < ApplicationController
     end
 
     def listing_params
-        params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet, :picture)
+        params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :city, :state, :deposit, :date_of_birth, :diet, :picture)
+    end
+
+    def set_breeds_and_sexes
+        @breeds=Breed.all 
+        @sexes= Listing.sexes.keys
     end
 
 end
